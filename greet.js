33cfed32a91b7d4ce64ba9badf6b42edeca6fe73
NonzwakaziMgxaji <nonzwakazimgxaji@gmail.com>
2021-04-28 09:02:23 +0200

    added functionality using js to greeting exercise

--- greet.js
+++ greet.js
@@ -1,30 +1,73 @@
 var nameEntered = document.querySelector(".userName");
 var radioSelected = document.querySelector(".radio");
-var greetBtn = document.querySelector(".greetBtn");
 var greet = document.querySelector(".greeting");
 var count = document.querySelector(".counter");
+var button = document.querySelector(".greetBtn");
+var reset = document.querySelector(".resetBtn");
 
-// var totalCount = 0;
-// var name = nameEntered.value;
-// var greets = greet.value;
+var existingNames;
+var namesGreeted = existingNames || {};
+var greetCounter = 0;
 
-greetBtn.addEventListener("click", function(){
-    var person = nameEntered.value;
+//check if this user was already greeted before
+function nameVal(name) {
+  if (namesGreeted[name] === undefined) {
+    namesGreeted[name] = 0;
+  }
+}
 
-    var rad = document.querySelector("input[name='language']:checked");
-    if (rad){
-        var selectedRad = rad.value;
-        if(person && selectedRad === "English"){
-            greets = "Hello," + person;
-        }
-        else if (person && selectedRad === "Afrikaaans"){
-            greets = "Goeie more," + person;
-        }
-        else{
-            greets = "Molo," + person;
-        }
+button.addEventListener("click", function () {
+  var greeting = greet.value;
+  nameVal(nameEntered.value);
+  
+  var rad = document.querySelector("input[name='language']:checked");
+  if (rad) {
+    var selectedRad = rad.value;
+    
+    if (selectedRad === "english") {
+      greeting = "Hello, " + nameEntered.value;
+    }
+    else if (selectedRad === "afrikaans") {
+      greeting = "Goeie môre, " + nameEntered.value;
+    }
+    else if (selectedRad === "isixhosa") {
+      greeting = "Molo, " + nameEntered.value;
     }
 
+    //localStorage to keep the names greeted
+    localStorage.setItem('names', JSON.stringify(namesGreeted));
 
+    //assign counter func(which returns the number of names greeted) to greetCounter
+    greetCounter = counter();
+  }
 
+  //display the greeting or contents of greet and HTML element 
+  greet.innerHTML = greeting;
+  count.innerHTML = greetCounter;
+
+  //clear textbox once button is clicked
+  nameEntered.value = "";
 });
+
+//retrieve the values stored in local storage
+if (localStorage['names']) {
+  existingNames = localStorage.getItem('names', JSON.parse(localStorage['names']));
+}
+
+// objects keys method returns an array of a given object's own enumerable property names
+function counter() {
+  var key = Object.keys(namesGreeted);
+  return key.length;
+}
+
+
+// if (localStorage['names']){
+//   greetCounter = Number(localStorage['names']);
+// }
+
+//resets counter to zero
+reset.addEventListener("click", function () {
+  location.reload();
+  localStorage.clear();
+});
+
